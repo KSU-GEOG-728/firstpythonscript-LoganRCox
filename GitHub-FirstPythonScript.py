5f749192265abe76fe142d8be273202516d742bb
LoganRCox <lrcox@ksu.edu>
2024-09-17 17:32:56 -0500

    Update GitHub-FirstPythonScript.py

--- GitHub-FirstPythonScript.py
+++ GitHub-FirstPythonScript.py
@@ -3,9 +3,9 @@
 
 """
     File name: GitHub-FirstPythonScript.py
-    Author: ENTER YOUR  NAME
-    Description:  ENTER BRIEF DESCRIPTION
-    Date created: ENTER TODAY'S DATE in MM/DD/YYYY FORMAT
+    Author: Logan Cox
+    Description:  Calculates total river miles within expanded ecoregion boundary.
+    Date created: 9/11/24
     Python Version: 3.9.16
 """
 
@@ -14,3 +14,20 @@ import arcpy
 arcpy.env.overwriteOutput = True
 
 # Set current workspace
+arcpy.env.workspace = "C:\\Users\\lrcox\\Documents\\GitHub\\firstpythonscript-LoganRCox\\GIS_Project\\GitHub-FirstPythonScript.gdb"
+
+#This line selects the Flint Hills ecoregion based off of it's name attribute
+selectEcoregion = arcpy.management.SelectLayerByAttribute('ks_ecoregions', 'NEW_SELECTION', "US_L3NAME = 'Flint Hills'")
+
+#This line makes a 10 km buffer around the Flint Hills ecoregion
+regionBuffer = arcpy.analysis.Buffer(selectEcoregion, 'FlintHills_Buffer', '10 Kilometers')
+
+#This line clips all of the ks rivers that are within the buffered ecoregion
+riverClip = arcpy.analysis.Clip('ks_major_rivers', regionBuffer, 'FH_River_Clip')
+
+#This line gets the total distance of the rivers in the clip
+distanceKM = arcpy.analysis.Statistics(riverClip, 'totalDistance', [["Shape_Length", "SUM"]], MS)
+
+#This line takes the total distance and converts it from kilometers to miles
+riverDistance_miles = distanceKM / 0.62137119
+
